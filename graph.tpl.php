133ba2d8e0e44eff43e9420e40e111392c4681d4
Mark Phillips <mbp@maple.gateway.2wire.net>
2012-03-24 18:12:03 -0400

    clean up displays

--- graph.tpl.php
+++ graph.tpl.php
@@ -10,5 +10,7 @@
  */
 ?>
 <div id="multigraph graph">
-  <?php print $graph; ?>
+   This is a placeholder for a graph of size <?php print $width; ?> X <?php print $height; ?>, with the 
+   following MUGL:
+   <textarea cols="60" rows="10" readonly="readonly"><?php print $mugl; ?></textarea>
 </div>
