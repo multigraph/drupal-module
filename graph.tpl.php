be49040072c300653a1c74c7dfa83c8f644160f7
Mark Phillips <mbp@maple.gateway.2wire.net>
2012-03-23 22:35:31 -0400

    initial import

---
+++ graph.tpl.php
@@ -0,0 +1,14 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Default theme implementation for rendering multigraph graph
+ *
+ * Available variables:
+ * - $graph:  the graph
+ */
+?>
+<div id="multigraph graph">
+  <?php print $graph; ?>
+</div>
