03c09bc6c1d8d6bc720d4afd77e3723c12e014b7
Mark Phillips <mphillip@unca.edu>
2012-03-26 22:15:23 -0400

    add flash code to graph.tpl.php

--- graph.tpl.php
+++ graph.tpl.php
@@ -10,7 +10,5 @@
  */
 ?>
 <div id="multigraph graph">
-   This is a placeholder for a graph of size <?php print $width; ?> X <?php print $height; ?>, with the 
-   following MUGL:
-   <textarea cols="60" rows="10" readonly="readonly"><?php print $mugl; ?></textarea>
+<?php print $flashcode; ?>
 </div>
